cee2f2ec33d73dbcdf011c424c7c3a9702dd809a
coozoo <realcoozoo@gmail.com>
2024-03-19 16:34:03 +0200

    add translation mockup
    thus will add nontranslated files to project

--- rf8000.pro
+++ rf8000.pro
@@ -4,7 +4,10 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 TARGET = qtrfpowermeter
 
+TRANSLATIONS = ./translations/$${TARGET}_en_US.ts ./translations/$${TARGET}_uk_UA.ts
+
 CONFIG += c++17
+CONFIG += lrelease
 
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
@@ -38,3 +41,13 @@ RESOURCES += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+binary.files += $$TARGET
+binary.path = /usr/bin
+translations.files += ./translations/$$files(.qm/*.qm,true)
+translations.path = /usr/share/$$TARGET
+icon.files += images/icon.png
+icon.path += /usr/share/icons
+desktop.files += $${TARGET}.desktop
+desktop.path += /usr/share/applications/
+INSTALLS += binary translations icon desktop
---
+++ translations/qtrfpowermeter_en_US.ts
@@ -0,0 +1,205 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Simulate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>dBm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>mW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>mVpp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>max dBm:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>max mW:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Correction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>buttonGroup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>dB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Frequency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MHz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Chart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset Charts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Range(min):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Flow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Write on fly data to CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>csv</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Frequency MHz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Correction dB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>After this time data on chart will move out so it will look like a flow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh Devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save charts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save charts as images to the log folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose output format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set image width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set image height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Write on fly data to csv file into log folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error incoming data, check device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Port: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> (</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>) </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> [Busy]</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
